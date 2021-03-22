7440abfc537a97550a9a999e05aa658cfded1b27
zakidze <zakidze@gmail.com>
2021-03-22 14:20:23 +0100

    message

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            add new blade
-        </div>
-    </div>
+<div class="wrapper">
+    <h1>create new pizza</h1>
+    <br>
+    <br>
+    <form action="/pizzas" methode="POST">
+        <label for="name">your name</label>
+        <input type="text" name="name" id="name">
+        <label for="type">tape</label>
+        <select name="type" id="type">
+            <option value="margarita">margarita</option>
+            <option value="hawain">hawain</option>
+            <option value="vvolcano">vvolcano</option>
+        </select>
+        <label for="type">base</label>
+        <select name="type" id="type">
+            <option value="think">this</option>
+            <option value="think">garlci</option>
+            <option value="think">crispy</option>
+        </select>
+        <input type="submit" value="order pizza">
+
+    </form>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+    <div class="top-right links">
+        @auth
+        <a href="{{ url('/home') }}">Home</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}">Register</a>
+        @endif
+        @endauth
+    </div>
     @endif
 
     <div class="content">
@@ -21,6 +21,8 @@
         <div class="title m-b-md">
             The North's Best Pizzas
         </div>
+        <a href="pizzas/create">create pizzas</a>
     </div>
+
 </div>
 @endsection
